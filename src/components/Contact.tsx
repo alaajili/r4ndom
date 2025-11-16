45caa117e0e5d4957ba4690e5cd539f67d738a97
Abdou <abderrahmanelaajili0@gmail.com>
2025-11-16 19:37:32 +0100

    feat: fix the env vars names

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -31,9 +31,9 @@ export function Contact() {
 
     try {
       // Get environment variables
-      const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
-      const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
-      const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+      const serviceId = import.meta.env.EMAILJS_SERVICE_ID;
+      const templateId = import.meta.env.EMAILJS_TEMPLATE_ID;
+      const publicKey = import.meta.env.EMAILJS_PUBLIC_KEY;
       
       console.log(serviceId, templateId, publicKey)
 
